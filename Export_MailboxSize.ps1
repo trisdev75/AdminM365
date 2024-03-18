9fb8eb1b0dc92a85ef1335919ff01780b6ac9db2
Tristan <55245589+trisdev75@users.noreply.github.com>
2024-03-18 11:23:20 +0100

    Add files Export_MailboxSize.ps1
    Add new script to repo.

---
+++ Export_MailboxSize.ps1
@@ -0,0 +1,56 @@
+﻿<#
+.SYNOPSIS
+    Export Mailboxsize for a user's list.
+
+.DESCRIPTION
+    User a CSV file containing userPrincipalName of users.
+
+.NOTES
+    File Name      : Export1_MailboxeSize.ps1
+    Author         : trisdev75
+    Created        : 18/03/2024
+    Last Modified  : NA
+    Version        : 1.0
+    PowerShell Version: 5.1
+    Changelog      : 
+        - Date: NA
+
+.PARAMETER Name
+    No Parameters needed
+
+.EXAMPLE
+    .\Export_MailboxSize.ps1
+    Create an output file with mailboxsize.
+#>
+
+
+# Main script execution
+
+# Connect To ExchangeOnline
+Connect-ExchangeOnline
+
+# Files
+$FilePath = #FILE TO IMPORT
+$OutputFile = #PATH TO OUTPUT
+
+# CSV
+#userPrincipalName;
+
+$Users = import-CSV $FilePath
+$Result = New-Object System.Collections.ArrayList
+
+foreach($user in $Users){
+    $UPN = $user.UPN
+    $Size = (Get-EXOMailboxStatistics -Identity $UPN).TotalItemSize
+    $MailboxSize = [PSCustomObject]@{
+            UPN = $UPN
+            TotalItemSize = $Size
+        }
+    $Null = $Result.Add($MailboxSize)
+
+}#For each
+
+$Result | Export-csv $OutputFile -Delimiter ";" -NoTypeInformation -Append
+
+# Disconnect
+Disconnect-ExchangeOnline
